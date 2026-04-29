6dc2a21178110d0a53ee1004cd14002562fa2d2d
Vanessa <vs6615302@gmail.com>
2026-04-28 22:41:57 -0300

    Organização de arquivos

--- meu-portfolio/src/componentes/home.tsx
+++ meu-portfolio/src/componentes/home.tsx
@@ -1,17 +1,19 @@
 function Home() {
   return (
-    <section>
+    <main>
       <div>
+        <img src="../src/img/FotoVanessa.jpg" alt="Foto de Vanessa" />
+        <h2>Vanessa Silva</h2>
+        <p>Desenvolvedora Front-End</p>
+
         <h1>Conteúdo do Projeto</h1>
         <p>Bem-vindo ao meu portfólio! Aqui você encontrará informações sobre meus projetos, habilidades e experiência profissional. Sinta-se à vontade para explorar e conhecer mais sobre mim.</p>
-
-        <div>
-          <img src="../src/img/FotoVanessa.jpg" alt="Foto de Vanessa"/>
-          <h2>Vanessa Silva</h2>
-          <p>Desenvolvedora Front-End</p>
-        </div>
       </div>
-    </section>
+
+      <section>
+        <h2>Projetos Recentes</h2>
+      </section>
+    </main>
   );
 }
 
