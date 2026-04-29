4652f3a08ed3a7f84791410e65ce9721d1cad0f8
Vanessa <vs6615302@gmail.com>
2026-04-28 22:06:52 -0300

    Atualização no home.tsx

--- meu-portfolio/src/componentes/home.tsx
+++ meu-portfolio/src/componentes/home.tsx
@@ -1,11 +1,17 @@
 function Home() {
   return (
-   <section>
-      <h1>Conteúdo do Projeto</h1>
-      <p>Bem-vindo ao meu portfólio! Aqui você encontrará informações sobre meus projetos, habilidades e experiência profissional. Sinta-se à vontade para explorar e conhecer mais sobre mim.</p>
+    <section>
+      <div>
+        <h1>Conteúdo do Projeto</h1>
+        <p>Bem-vindo ao meu portfólio! Aqui você encontrará informações sobre meus projetos, habilidades e experiência profissional. Sinta-se à vontade para explorar e conhecer mais sobre mim.</p>
 
-      
-   </section>
+        <div>
+          <img src="../src/img/FotoVanessa.jpg" alt="Foto de Vanessa"/>
+          <h2>Vanessa Silva</h2>
+          <p>Desenvolvedora Front-End</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
