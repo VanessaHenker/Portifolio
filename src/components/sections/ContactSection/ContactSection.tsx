22f9aba22ef666e4a7c26b854fe8a030f94156c6
Vanessa <vs6615302@gmail.com>
2026-05-01 19:03:16 -0300

    feat(contact): inicia ContactSection com CSS

--- src/components/sections/ContactSection/ContactSection.tsx
+++ src/components/sections/ContactSection/ContactSection.tsx
@@ -1,11 +1,29 @@
-
 function ContactSection() {
   return (
-    <section>
-      <h2>Contatoss</h2>
-      <form action="">
-        <input placeholder="Nome" type="text" />
-        <textarea placeholder="Digite sua mensagem" name="" id=""></textarea>
+    <section className="contact">
+      <h2>Contato</h2>
+
+      <form className="contact__form">
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          required
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+
+        <textarea
+          name="message"
+          placeholder="Digite sua mensagem"
+          required
+        ></textarea>
+
         <button type="submit">Enviar</button>
       </form>
     </section>
