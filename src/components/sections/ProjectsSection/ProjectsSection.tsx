5c3b98544e501eeea6de20042c611fd335031035
Vanessa <vs6615302@gmail.com>
2026-05-03 13:41:27 -0300

    feat: adiciona links dos projetos no ProjectsSection

--- src/components/sections/ProjectsSection/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection/ProjectsSection.tsx
@@ -1,20 +1,25 @@
 import ProjectCard from "../../../cards/ProjectCard";
 import "./ProjectsSection.css";
-import projeto1 from "./img/Projeto1.png";
+import projeto1 from "./img/PizzaBliss.png";
+import projeto2 from "./img/TelaLogin.png";
 
 function ProjectsSection() {
   const projects = [
     {
       id: 1,
       image: projeto1,
-      title: "Loja de Carros",
-      description: "Loja virtual de venda de carros.",
+      title: "Pizza Bliss",
+      description:
+        "Cardápio digital de pizzaria fictícia com organização modular de categorias e itens, desenvolvido para exibição de produtos de forma simples e responsiva.",
+      link: "https://menu-bliss.vercel.app/",
     },
     {
       id: 2,
-      image: projeto1,
-      title: "Loja de Carros",
-      description: "Loja virtual de venda de carros.",
+      image: projeto2,
+      title: "Tela Login",
+      description:
+        "Tela de login simples para acesso a um sistema de gerenciamento, desenvolvida com JavaScript, sem validação de dados ou integração com backend.",
+      link: "https://tela-login-one-alpha.vercel.app/",
     },
   ];
 
@@ -29,6 +34,7 @@ function ProjectsSection() {
             image={project.image}
             title={project.title}
             description={project.description}
+            link={project.link}
           />
         ))}
       </div>
